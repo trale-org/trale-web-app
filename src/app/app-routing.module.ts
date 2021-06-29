26abc04d726ec80328167eb8cc0cbd7c249f2fc2
MauritsvanderZee <maurits@van-der-zee.de>
2021-06-30 00:57:03 +0200

    define route for settings index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule}               from '@angular/core';
+import {RouterModule, Routes}   from '@angular/router';
+import {SettingsIndexComponent} from './settings/settings-index/settings-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'settings', component: SettingsIndexComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
