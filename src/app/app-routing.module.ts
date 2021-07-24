68ff5bb1ebcc3bb573594341d0aba8831ed39a6e
mauritsvanderzee <maurits@van-der-zee.de>
2021-07-24 09:45:09 +0200

    add routes for setup index and setup security level

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule}               from '@angular/core';
 import {RouterModule, Routes}   from '@angular/router';
 import {SettingsIndexComponent} from './settings/settings-index/settings-index.component';
+import {ChooseSecurityLevelComponent} from "./setup/choose-security-level/choose-security-level.component";
 
 const routes: Routes = [
   {path: 'settings', component: SettingsIndexComponent},
+  {path: 'setup', component: ChooseSecurityLevelComponent},
+  {path: 'setup/security-level', component: ChooseSecurityLevelComponent},
 ];
 
 @NgModule({
