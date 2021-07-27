bafc44de65b2ac04a49076665b2541df5ebd8e93
MauritsvanderZee <maurits@van-der-zee.de>
2021-07-27 14:28:51 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import {NgModule}               from '@angular/core';
 import {RouterModule, Routes}   from '@angular/router';
 import {SettingsIndexComponent} from './settings/settings-index/settings-index.component';
+import {WelcomeComponent}       from './welcome-stepper/welcome/welcome.component';
 
 const routes: Routes = [
+  {path: '', component: SettingsIndexComponent},
+  {path: 'welcome', component: WelcomeComponent},
   {path: 'settings', component: SettingsIndexComponent},
 ];
 
