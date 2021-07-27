bc109e9471bb2ab6972ba5b277d27b39da5e1c02
MauritsvanderZee <maurits@van-der-zee.de>
2021-07-27 14:29:03 +0200

    add router import; add condition to header;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router}    from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls:   ['./app.component.scss']
 })
 export class AppComponent {
   title = 'trale-web-app';
+  public router: string;
+
+  constructor(
+    private _router: Router,
+  ) {
+    this.router = _router.url;
+  }
+
 }
